0c1cb2deabc6028e4496471256fafa4db12621eb
Juan Oxmar <joxmar.ramirez@gmail.com>
2021-01-16 14:25:26 -0800

    Start of Styled Buttong

--- client/src/components/Button/index.jsx
+++ client/src/components/Button/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import classes from './Button.module.css';
 
 export default function Button({ children }) {
   return (
-    <button>
-      {children}
+    <button type="button" className={classes.btn}>
+      <span className={classes.text}>
+        {children}
+      </span>
     </button>
   );
 }
+
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+};
