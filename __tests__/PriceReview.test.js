1be5859d622641ee1689eaccb4abf9d5304664a3
Juan Oxmar <joxmar.ramirez@gmail.com>
2021-01-18 15:34:50 -0800

    Price and Review component creation. Updated App to render PriceReview.

---
+++ __tests__/PriceReview.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import PriceReview from '../client/src/components/PriceReview';
+
+describe('Price Review', () => {
+  it('renders the Price Review component', () => {
+    render(<PriceReview price={150} avg={4.85} total={55} />);
+    expect(screen.getByText('$150')).toBeInTheDocument();
+  });
+});
