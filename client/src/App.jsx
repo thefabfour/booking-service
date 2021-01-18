521e761688fcdad1576494e351bef7c221ed017b
Juan Oxmar <joxmar.ramirez@gmail.com>
2021-01-18 14:04:50 -0800

    Updated App component layout with button. Styling added to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import Button from './components/Button';
+import classes from './App.module.css';
+
 export default function App() {
   return (
-    <div>
-      Initial Commit
-      <div>Test</div>
+    <div className={classes.container}>
+      <div>Price/Night and Overall Rating Components</div>
+      <div>Earliest Availability Component</div>
+      <div>Check In / Checkout Component</div>
+      <Button>Check Availability</Button>
     </div>
   );
 }
