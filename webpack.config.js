e6fdcad2c57ad451d792afbfd3d1e4e8f63c442f
Juan Oxmar <joxmar.ramirez@gmail.com>
2021-01-27 15:29:44 -0800

    Changed css config in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
             options: {
               importLoaders: 1,
               modules: {
-                localIdentName: '[name]__[local]--[hash:base64:5]',
+                localIdentName: '[name]__[local]--[hash:base64:6]',
               },
             },
           },
