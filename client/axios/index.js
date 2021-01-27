e3bd7890e802ab62dbe1db0ad1b6a4b4955920c7
Juan Oxmar <joxmar.ramirez@gmail.com>
2021-01-26 17:04:54 -0800

    Changed how the params are searched

--- client/axios/index.js
+++ client/axios/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const id = window.location.search.split('?propertyId=')[1];
+const id = new URLSearchParams(window.location.search).get('propertyId');
 
 const instance = axios.create({
   baseURL: `/api/rooms/${id}/booking`,
